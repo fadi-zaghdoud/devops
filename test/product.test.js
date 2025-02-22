ecc16ba97750024a3bae2d9ae35d0a113a5bc3eb
Zaghdoud Fadi <fadi.zaghdoud@esprit.tn>
2025-02-22 06:47:57 +0100

    Update product.test.js

--- test/product.test.js
+++ test/product.test.js
@@ -4,40 +4,40 @@ const chai = require('chai');
 const expect = chai.expect;
 const should = chai.should();
 const chaiHttp = require('chai-http');
-const server = require('../server'); // This exports the server instance from server.js
+const server = require('../server');
 const Product = require('../product');
+
 const mongoose = require('mongoose');
+
 
 chai.use(chaiHttp);
 
+
+
+
 describe('Test', () => {
 
-  it('should POST a valid product', (done) => {
+
+
+
+
+it('should POST a valid product', (done) => {
+        
     let product = {
-      name: "Test Product",
-      price: 100,
-      quantity: 20
-    };
-
-    chai.request(server)
-      .post('/api/products')
-      .send(product)
-      .end((err, res) => {
-        res.should.have.status(201);
-        done();
-      });
-  });
-
-  // After all tests, attempt to close the server.
-  after(function(done) {
-    // Check if the server is still running before closing
-    if (server && server.listening) {
-      server.close((err) => {
-        // If an error occurs (e.g., server already closed), ignore it.
-        done();
-      });
-    } else {
-      done();
+        name: "Test Product",
+        price: 100,
+        quantity: 20
     }
-  });
+    chai.request(server)
+    .post('/api/products')
+    .send(product)
+    .end((err, res) => {
+        res.should.have.status(201);
+        
+        done();
+
+    });
+
+});
+
 });
