c9fb0a257d34257088e3bb2c3744215997e1d798
Zaghdoud Fadi <fadi.zaghdoud@esprit.tn>
2025-02-22 06:10:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ server.use("/api", productRoutes); //CRUD routes
 
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, function () {
-  console.log("Server is running on port:  " + PORT);
+  console.log("Server is running on port: " + PORT);
 });
+
 
 module.exports = server;
